b6630c3eb49af172eaea4f37fd1d05042a7fd347
Davi Marques <davimarques22558800@gmail.com>
2024-08-27 19:39:58 -0300

    Operadores e operações

---
+++ jsbasico/aula2.js
@@ -0,0 +1,30 @@
+let n1 = 10
+let n2 = 2
+let n3 = "2"
+
+document.write(`N1: ${n1} <br>`)
+document.write(`n2: ${n2} <br>`)
+document.write(`N3: ${n3} <br>`)
+
+console.log(n1 + n2)
+console.log(n1 - n2)
+console.log(n1 * n2)
+console.log(n1 / n2)
+console.log(n1 % 2)
+
+console.log(n2 == n3) //igual a
+console.log(n2 === n3) //identico a (tipos iguais)
+console.log(n2 != n3) // diferente de não=
+console.log(n2 !== n3) // não é 
+
+console.log(n1 > n2)
+console.log(n1 < n2)
+console.log(n1 >= n2)
+console.log(n1 <= n2)
+
+let a = true
+let b = false
+console.log(a && b) // e and
+console.log(a || b) // ou or
+console.log(!a) //nao not negação
+console.log(!b) //nao not negação
