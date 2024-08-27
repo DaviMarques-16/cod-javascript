f276068497f28028f32c34878da64422a04cf699
Davi Marques <davimarques22558800@gmail.com>
2024-08-27 19:08:29 -0300

    Variáveis e tipos

---
+++ jsbasico/aula1.js
@@ -0,0 +1,27 @@
+document.write("Olá mundo!")
+console.log("Outro olá para o mundo")
+// alert("Olá agora alertando!")
+
+let mensagem = "estou enviando uma msg"
+let mensagem2 = "estou enviando a segunda mensagem"
+let meu_peso = 78.6
+let minhaAltura = 1.79
+let minha_idade = 16
+let ehdoador = false
+let teste = null
+
+//constantes
+const PI = 3.1415
+const Taxa = 0.5
+
+teste = "algum conteúdo"
+
+document.write("<p> Mensagem: " + mensagem + "</p>")
+document.write(`<p> Mensagem 2: ${mensagem2} </p>`)
+document.write(`<p> Peso: ${meu_peso} </p>`)
+document.write(`<p> Altura: ${minhaAltura} </p>`)
+document.write(`<p> Idade: ${minha_idade} </p>`)
+document.write(`<p> é doados?: ${ehdoador} </p>`)
+document.write(`<p> valor do pi: ${PI} </p>`)
+document.write(`<p> O valor da taxa é ${Taxa}</p>`)
+
