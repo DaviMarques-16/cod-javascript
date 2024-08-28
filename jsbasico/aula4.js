a6cb1ca9ee76553e73b65eb39d3ae53511477d89
Davi Marques <davimarques22558800@gmail.com>
2024-08-28 19:04:02 -0300

    Objetos em js

---
+++ jsbasico/aula4.js
@@ -0,0 +1,45 @@
+// Criar objeto
+let pessoa= {
+    nome :  'Davi',
+    idade : 16,
+    peso : 78,
+    altura : 1.79
+}
+
+let produtos = {
+    descricao : [],
+    preco : []
+}
+
+const carros = {
+    marca : ['Ford', 'Fiat', 'GM'],
+    modelo: ['Ka', 'Uno', 'Corsa'],
+    ano : [1999, 2005, 2010]
+}
+
+/*Acessar propriedade usando .
+pessoa.nome 
+pessoa.idade
+pessoa.peso  */
+
+//também dá ['chave']
+
+let numquebrado = 27.8372623839
+
+//  .toFixed(x)
+// Limita casas decimais
+
+console.log(numquebrado.toFixed(2))
+
+//Alteração
+pessoa.nome = "Davi Marques"
+produtos.descricao = ['Arroz']
+produtos.preco = [4.99] 
+
+//Usando spreed para adicionar além do primeiro.
+produtos.descricao = [...produtos.descricao, 'Feijão', 'Trigo']
+produtos.preco = [...produtos.preco, 9.99 , 4.79]
+
+carros.marca = [...carros.marca, 'WV']
+carros.modelo = [...carros.modelo, 'Fusca']
+carros.ano = [...carros.ano, '1979']
